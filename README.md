52a08f989a55c40464fd38f847797edd4e79576b
bcopy <500789+bcopy@users.noreply.github.com>
2021-01-10 17:28:30 +0100

    Add test client

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# microsquad
-A Microbit orchestration library based on Bitio
+# MicroSquad (a.k.a uSquad)
+
+A Microbit orchestration library based on [Bitio](https://github.com/AdventuresInMinecraft/bitio) : Using a single Microbit as a gateway, control remote Microbits via the radio.
+
+![Microbit](https://microbit-micropython.readthedocs.io/en/v1.0.1/_images/happy.png)
+
+**Basic functionalities include :**
+* Broadcast, group and unicast messaging
+* Assigning session identifiers (will be resent with each message from the client)
+* Remotely controlling displays
+* Requesting remote sensor readings (buttons, gyroscope, compass, temperature, votes etc...)
+
+
+# Dependencies
+
+For the **uSquad** Gateway :
+* Python3
+* [https://github.com/AdventuresInMinecraft/Bitio](https://github.com/AdventuresInMinecraft/bitio)
+* A Microbit running the bitio firmware
+
+For the **uSquad** clients :
+* The provided **uSquad** firmware to upload on each Microbit.
---
+++ modules/client/src/main/micropython/main.py
@@ -0,0 +1,91 @@
+from microbit import display,Image,sleep, button_a
+import radio
+
+radio.config(length=251, queue=6, channel=12, group=1)
+radio.on()
+
+############################
+#  uLineProtocol
+
+# Very simple and probably buggy line protocol parser
+# Do not reuse !
+
+def _pop_head_or_none(arr, peek_only = False):
+    if arr and len(arr)>0:
+        if peek_only:
+          return arr[0]
+        else:
+          return arr.pop(0)
+    else:
+        return None
+
+
+def parse_lineprotocol_message(msg):
+    measurement = None
+    tags = {}
+    values = {}
+    timestamp = None
+
+    fragments = msg.split(" ")
+    fragment = _pop_head_or_none(fragments)
+    if fragment is not None:
+        measurementFragments = fragment.split(",")
+        if len(measurementFragments) > 0:
+            measurement = measurementFragments[0]
+        if  len(measurementFragments) > 1:
+            for tagFragment in measurementFragments[1:]:
+                tagKV = tagFragment.split("=")
+                tags[tagKV[0]] = tagKV[1]
+        
+    fragment = _pop_head_or_none(fragments,True)
+    if fragment is not None:
+        if("=" in fragment):
+            fragment = _pop_head_or_none(fragments)
+            valuesFragment = fragment.split(",")
+            for valueFragment in map(lambda v: v.split("="),valuesFragment):
+                values[valueFragment[0]] = valueFragment[1]
+
+    fragment = _pop_head_or_none(fragments)
+    if fragment is not None:
+        timestamp = int(fragment)
+        
+    return (measurement, tags, values, timestamp)
+
+def serialize_lineprotocol_message(measurement, tags= None, values=None, timestamp=None): 
+    result = measurement
+    if tags is not None:
+      result += (','.join('{}="{}"'.format(key, value) for key, value in tags.items())) + " "
+    if values is not None:
+      result += (','.join('{}={}'.format(key, value) for key, value in values.items())) + " "
+    if timestamp is not None:
+      result += timestamp
+    else:
+      result += microbit.running_time()
+    return result
+
+# uLineProtocol
+############################
+
+############################
+
+
+# Create the "flash" animation frames.
+full = Image()
+full.invert()
+flash = [(full * (i/9)) for i in range(9, -1, -1)]
+
+display.show(Image.TARGET)
+while True:
+    measurement = None
+    if button_a.was_pressed():
+        measurement="flash"
+
+    incoming = radio.receive()
+    if incoming is not None:
+      measurement,tags,values = parse_lineprotocol_message(incoming)
+
+    if measurement == 'flash':
+        display.show(flash, delay=50, wait=True)
+
+    sleep(200)
+    display.show(Image.TARGET)
