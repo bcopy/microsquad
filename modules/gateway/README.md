d13bc397dd7118cfea9f780bf179bbdfb53121af
bcopy <500789+bcopy@users.noreply.github.com>
2021-06-17 17:11:27 +0200

    Add instruction for virtual env

---
+++ modules/gateway/README.md
@@ -0,0 +1,12 @@
+# How to initialize the environment 
+
+```
+python3 -m venv usquad-venv
+```
+
+# How to use
+
+Execute
+```
+. ./setup-venv.sh
+```
---
+++ modules/gateway/setup-venv.sh
@@ -0,0 +1,4 @@
+source usquad-venv/bin/activate
+pip3 install -r requirements.txt
+export PYTHONPATH=`pwd`/src/main/python:`pwd`/src/test/python:$PYTHONPATH
+
