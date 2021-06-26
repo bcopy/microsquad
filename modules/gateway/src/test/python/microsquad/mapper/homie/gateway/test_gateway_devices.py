115e23068c30166363fb21802606ae8506a2a08b
bcopy <500789+bcopy@users.noreply.github.com>
2021-06-26 10:56:14 +0200

    Rename private methods

--- modules/gateway/src/test/python/microsquad/mapper/homie/gateway/test_gateway_devices.py
+++ modules/gateway/src/test/python/microsquad/mapper/homie/gateway/test_gateway_devices.py
@@ -12,32 +12,32 @@ class SimpleTest(unittest.TestCase):
         self.gateway.start()
 
     def test_add_player(self):
-        self.gateway.player_manager.add_player("01")
+        self.gateway._player_manager.add_player("01")
         self.assertIsNotNone(self.gateway.get_node("player-01"))
         self.assertIsNone(self.gateway.get_node("player-02"))
-        self.gateway.player_manager.add_player("02")
+        self.gateway._player_manager.add_player("02")
         self.assertIsNotNone(self.gateway.get_node("player-02"))
         self.assertIsNotNone(self.gateway.get_node("player-02").get_property("nickname"))
         self.assertIsNotNone(self.gateway.get_node("player-02").get_property("skin"))
 
     def test_add_remove_teams(self):
-        self.gateway.team_manager.add_team("blue")
-        self.assertEqual(self.gateway.team_manager.get_property("list").value,'["blue"]')
-        self.gateway.team_manager.remove_team("blue")
-        self.assertEqual(self.gateway.team_manager.get_property("list").value,'[]')
+        self.gateway._team_manager.add_team("blue")
+        self.assertEqual(self.gateway._team_manager.get_property("list").value,'["blue"]')
+        self.gateway._team_manager.remove_team("blue")
+        self.assertEqual(self.gateway._team_manager.get_property("list").value,'[]')
         
     def test_add_remove_player_to_team(self):
-        self.gateway.player_manager.add_player("susan")
-        self.gateway.player_manager.add_player("roger")
-        self.gateway.team_manager.add_team("orange")
+        self.gateway._player_manager.add_player("susan")
+        self.gateway._player_manager.add_player("roger")
+        self.gateway._team_manager.add_team("orange")
         self.assertIsNotNone(self.gateway.get_node("team-orange"))
-        self.gateway.team_manager.add_player("orange:susan")
-        self.assertEqual(self.gateway.team_manager.get_property("list").value,'["orange"]')
-        self.assertEqual(self.gateway.team_manager.get_property("list-players").value,'{"orange":["susan"]}')
-        self.gateway.team_manager.add_player("orange:roger")
-        self.assertEqual(self.gateway.team_manager.get_property("list-players").value,'{"orange":["susan","roger"]}')
-        self.gateway.team_manager.remove_player("orange:susan")
-        self.assertEqual(self.gateway.team_manager.get_property("list-players").value,'{"orange":["roger"]}')
+        self.gateway._team_manager.add_player("orange:susan")
+        self.assertEqual(self.gateway._team_manager.get_property("list").value,'["orange"]')
+        self.assertEqual(self.gateway._team_manager.get_property("list-players").value,'{"orange":["susan"]}')
+        self.gateway._team_manager.add_player("orange:roger")
+        self.assertEqual(self.gateway._team_manager.get_property("list-players").value,'{"orange":["susan","roger"]}')
+        self.gateway._team_manager.remove_player("orange:susan")
+        self.assertEqual(self.gateway._team_manager.get_property("list-players").value,'{"orange":["roger"]}')
         
 
 if __name__ == '__main__':
