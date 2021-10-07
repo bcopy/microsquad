4e147281cc7daa64989ddb7888c298b74816c30a
bcopy <500789+bcopy@users.noreply.github.com>
2021-10-07 11:16:18 +0200

    Add player scale and orientation property

--- modules/gateway/src/main/python/microsquad/mapper/homie/gateway/node_player.py
+++ modules/gateway/src/main/python/microsquad/mapper/homie/gateway/node_player.py
@@ -3,6 +3,8 @@ import logging
 from homie.node.property.property_string import Property_String
 from homie.node.property.property_datetime import Property_DateTime
 from homie.node.property.property_integer import Property_Integer
+from homie.node.property.property_float import Property_Float
+
 from homie.node.node_base import Node_Base
 
 logger = logging.getLogger(__name__)
@@ -25,6 +27,8 @@ class NodePlayer(Node_Base):
       self.add_property(Property_String(self,   id="nickname",           name="nickname"))
       self.add_property(Property_String(self,   id="skin",               name="skin"))
       self.add_property(Property_Integer(self,  id="order",              name="order", settable=False))
+      self.add_property(Property_Float(self,    id="scale",              name="Scale", settable=False))
+      self.add_property(Property_Float(self,    id="orientation",        name="Orientation", settable=False))
       self.add_property(Property_String(self,   id="say",                name="say"))
       self.add_property(Property_DateTime(self, id="say-start",          name="say start"))
       self.add_property(Property_Integer(self,  id="say-duration",       name="say duration", settable=False))
