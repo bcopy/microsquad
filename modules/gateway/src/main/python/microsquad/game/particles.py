aff092b3eb2d9fa7a6937b381b833f33269cdccc
bcopy <500789+bcopy@users.noreply.github.com>
2021-10-08 01:32:39 +0200

    Use simplified protocol

--- modules/gateway/src/main/python/microsquad/game/particles.py
+++ modules/gateway/src/main/python/microsquad/game/particles.py
@@ -2,12 +2,13 @@ import enum
 
 @enum.unique
 class PARTICLE(enum.Enum):
-    PROTON = ("proton","09990:99399:99999:99990:99000", "30903:30003:30003:30003:30003;30903:30903:30003:30003:30003;30903:30903:30903:39003:90003")
-    ELECTRON = ("electron","90009:09090:00000:99999:90909", "30903:30003:30003:30003:30003;30903:30903:30003:30003:30003;30903:30903:30903:30093:30009")
-
-    def __init__(self, identifier : str, display : str, trajectory : str) -> None:
-        self.display = display
-        self.trajectory = trajectory
+    ELECTRON = ("electron",0)
+    PROTON = ("proton",1)
+    PHOTON = ("photon", 2)
+    NEUTRON = ("neutron",3)
+    
+    def __init__(self, identifier : str, idx : int) -> None:
+        self.idx = idx
         self.identifier = identifier
     
 
