dc886abe108c733e577a042974a7d29ed57c47fd
bcopy <500789+bcopy@users.noreply.github.com>
2022-06-06 19:15:25 +0200

    Add websocket support to test broker

--- modules/client-web-library/src/__tests__/client.test.ts
+++ modules/client-web-library/src/__tests__/client.test.ts
@@ -1,14 +1,30 @@
-import { MicrosquadClient } from '../lib/client';
+// import { MicrosquadClient } from '../lib/client';
 
 import { of,from } from 'rxjs';
+
+
 
 const aedes = require('aedes')()
 const httpServer = require('http').createServer()
 const ws = require('websocket-stream')
+const port = 8888
+httpServer.listen(port, function () {
+  console.log('websocket server listening on port ', port)
+})
 
-const aedesServer = ws.createServer({ server: httpServer }, aedes.handle)
+const aedesServer = ws.createServer({ server: httpServer }, aedes.handle);
+
+// var mqtt = require('mqtt');
+// var client = mqtt.connect('ws://localhost:8888');
+
+// client.on('connect', function () {
+//   console.log("Client connected !");
+//   // client.publish('test', 'Hello mqtt');
+// })
+
 
 afterAll(() => {
+  httpServer.close();
   aedes.close();
 });
 
@@ -31,12 +47,15 @@ test('the observable interval emits 100 then 200 then 300', done => {
   })
 });
 
-test('client subscribe ok', () => {
-  var client = new MicrosquadClient("ws://localhost:8888","jest-client-ID", "/")
+test('client subscribe ok', done => {
+  // var client = new MicrosquadClient("ws://localhost:8888","jest-client-ID", "/")
+  // // let topic = MicrosquadClient.topic(`test/topic`)
 
+  // client.playerSubject$.subscribe( data => {
+  //   expect(data).toBe('hello');
+  //  
+  // });
   expect(4).toBe(4);
+  done();
 });
 
-// test('power', (t) => {
-//   t.is(power(2, 4), 16);
-// });
