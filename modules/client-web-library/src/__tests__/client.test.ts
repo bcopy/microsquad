3766af2b0472ec3c5a241d4a2562b671f1947cec
bcopy <500789+bcopy@users.noreply.github.com>
2022-06-06 18:35:12 +0200

    Add aedes for embedded mqtt testing

--- modules/client-web-library/src/__tests__/client.test.ts
+++ modules/client-web-library/src/__tests__/client.test.ts
@@ -1,8 +1,39 @@
-// import test from 'ava';
-
 import { MicrosquadClient } from '../lib/client';
 
-test('client ok', () => {
+import { of,from } from 'rxjs';
+
+const aedes = require('aedes')()
+const httpServer = require('http').createServer()
+const ws = require('websocket-stream')
+
+const aedesServer = ws.createServer({ server: httpServer }, aedes.handle)
+
+afterAll(() => {
+  aedes.close();
+});
+
+test('the observable emits hello', done => {
+  of('hello').subscribe( data => {
+    expect(data).toBe('hello');
+    done();
+  });
+});
+
+test('the observable interval emits 100 then 200 then 300', done => {
+  let last = 100;
+  from([100, 200, 300])
+  .subscribe({
+      next: val => {
+          expect(val).toBe(last)
+          last += 100
+        },
+      complete: () => done(),
+  })
+});
+
+test('client subscribe ok', () => {
+  var client = new MicrosquadClient("ws://localhost:8888","jest-client-ID", "/")
+
   expect(4).toBe(4);
 });
 
